9cbcbfa724c6d569d6b28857337e16905da7f44a
David Hackro <hackro91@gmail.com>
2017-10-26 17:12:46 -0500

    add dependencies in external file

--- MyFirstProfessionalApp/app/src/main/java/com/tutorials/hackro/myfirstprofessionalapp/ListProfilesActivity.java
+++ MyFirstProfessionalApp/app/src/main/java/com/tutorials/hackro/myfirstprofessionalapp/ListProfilesActivity.java
@@ -1,8 +1,7 @@
 package com.tutorials.hackro.myfirstprofessionalapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 public class ListProfilesActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class ListProfilesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_profiles);
-        Log.e("nhdbjhf",BuildConfig.BASE_URL);
     }
 }
